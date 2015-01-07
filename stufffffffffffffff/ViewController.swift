f81a165c56c52288654bfd2be06d5c8968728d8c
Jude Walinder <judewalinder@CGTMAED-iMac-9.local>
2015-01-07 08:39:42 -0700

    Initial Commit

---
+++ stufffffffffffffff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stufffffffffffffff
+//
+//  Created by Jude Walinder on 1/7/15.
+//  Copyright (c) 2015 Jude Walinder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
